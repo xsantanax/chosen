6e2de9a183df8414e90deac2f96c2a543db9cdcf
Rafael Santana <rafaelsantana111@gmail.com>
2023-07-16 14:27:07 -0300

    add votes and results routes

---
+++ app/results/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css'
+
+export default function Results() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.description}>Results Page</div>
+    </div>
+  )
+}
---
+++ app/votes/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css'
+
+export default function Votes() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.description}>Votes Page</div>
+    </div>
+  )
+}
