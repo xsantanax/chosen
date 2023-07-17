24ed47cd869a43c90805b481a1189583c9032cfe
Rafael Santana <rafaelsantana111@gmail.com>
2023-07-16 23:31:14 -0300

    VotesProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import styles from './page.module.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { VotesProvider } from '@/hooks/useVotes'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,9 +21,11 @@ export default function RootLayout({
     <html lang='en'>
       <body className={inter.className}>
         <Header />
-        <main className={styles.main}>
-          <div className={styles.container}>{children}</div>
-        </main>
+        <VotesProvider>
+          <main className={styles.main}>
+            <div className={styles.container}>{children}</div>
+          </main>
+        </VotesProvider>
       </body>
     </html>
   )
